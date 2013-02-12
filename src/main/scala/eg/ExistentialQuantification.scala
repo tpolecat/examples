9bc2d159e59eb751a3d46174c94757ea9ff7587a
Rob Norris <rob_norris@mac.com>
2013-02-12 15:48:03 -0800

    updated existential

--- src/main/scala/eg/ExistentialQuantification.scala
+++ src/main/scala/eg/ExistentialQuantification.scala
@@ -7,10 +7,22 @@ object ExistentialQuantification extends App {
 
   case class Foo[A](a: A)
 
+  // Each pair must have the same A in both slots, but the pairs can differ
+  // one to the next.
   def f(as: (Foo[A], A) forSome { type A }*) = 1
 
+  // Thus
   f((Foo(1), 2), (Foo("bar"), "baz"))
-//f((Foo(1), 2), (Foo("bar"), "baz"), (Foo("qux"), 3)) // doesn't compile
+  
+  // This doesn't compile (but will if Foo is covariant in A)
+  // f((Foo(1), 2), (Foo("bar"), "baz"), (Foo("qux"), 3)) 
 
+  // Works with Set, which is invariant
+  val s:Set[(Foo[A], A) forSome { type A }] = Set((Foo(1), 2), (Foo("bar"), "baz"))
+  // val s:Set[(Foo[A], A) forSome { type A }] = Set((Foo(1), 2), (Foo("bar"), "baz"))
+
+  // Doesn't work at all with List, which is covariant
+  // val x:List[(Foo[A], A) forSome { type A }] = List((Foo(1), 2), (Foo("bar"), "baz"))
+  
 }
 
