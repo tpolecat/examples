d8e86fcf77a690589b4a96936fe37f33309f0f03
Rob Norris <rob_norris@mac.com>
2013-02-12 20:08:32 -0800

    removed unused code

--- src/main/scala/eg/Kleisli.scala
+++ src/main/scala/eg/Kleisli.scala
@@ -10,7 +10,6 @@ import scalaz.std.AllInstances._
 object KleisliTest extends App {
 
   // Kleisli is aliased as ReaderT
-
   def foo[M[+_]: Monad]: Kleisli[M, String, Int] = for {
     s <- ask[M, String]
   } yield s.length
@@ -18,15 +17,5 @@ object KleisliTest extends App {
   println(foo[Id].run("foo"))
   println(foo[Option].run("foo"))
   println(foo[List].run("foo"))
-
-  
-  implicit class Foo[A](as:Seq[A]) {
-    def randomIndex = util.Random.nextInt(as.length)
-    def randomElement:Option[A] = if (as.isEmpty) None else Some(as(randomIndex))
-  }
-  
-  println(new Foo(List(1,2,3,4,5)).randomElement)
- 
-  
   
 }
