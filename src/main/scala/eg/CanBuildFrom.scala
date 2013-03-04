cc3c872068e92caf9c532fc690a6a5683cf77dec
Rob Norris <rob_norris@mac.com>
2013-03-03 18:16:02 -0800

    cbf example

--- src/main/scala/eg/CanBuildFrom.scala
+++ src/main/scala/eg/CanBuildFrom.scala
@@ -9,8 +9,8 @@ object CanBuildFromExample {
 
   // thanks to non/d_m and tixxit
   // https://github.com/non/spire/blob/2.10.0/core/src/main/scala/spire/package.scala#L350
-  def foo[A, CC[A] <: Iterable[A]](as: CC[A])(implicit ct: ClassTag[A], cbf: CanBuildFrom[CC[A], A, CC[A]]): CC[A] = {
-    as.toArray.to[CC] // convert to an array and back to original type
+  def foo[A: ClassTag, C[A] <: Traversable[A]](as: C[A])(implicit cbf: CanBuildFrom[C[A], A, C[A]]): C[A] = {
+    as.toArray.to[C] // convert to an array and back to original type
   }
 
   val x: List[Int] = foo(List(1, 2, 3))
