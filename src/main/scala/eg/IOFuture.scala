ee08e2efbb860f21ca44629a01fa1bf15371c5f1
Rob Norris <rob_norris@mac.com>
2013-04-29 21:31:06 -0700

    updated IOFuture

--- src/main/scala/eg/IOFuture.scala
+++ src/main/scala/eg/IOFuture.scala
@@ -9,35 +9,35 @@ import scala.concurrent.duration.Duration
 import scalaz.Bind
 import scalaz.syntax.monad._
 
-// A future that you can only mess with inside of IO
+// Wrap a future such that you can only see it in IO
 final class IOFuture[+A] private (f: Future[A])(implicit ec: ExecutionContext) {
 
-  def map[B](g: A => B): IOFuture[B] = 
+  def map[B](g: A => B): IOFuture[B] =
     new IOFuture(f.map(g))
 
   def flatMap[B](g: A => IOFuture[B]): IOFuture[B] =
     new IOFuture(Future(Await.result(f.map(g), Duration.Inf).await.unsafePerformIO))
 
-  def await: IO[A] = 
+  def await: IO[A] =
     IO(Await.result(f, Duration.Inf))
 
-  def onComplete(a: A => IO[Unit]): IO[Unit] = 
+  def onSuccess(a: A => IO[Unit]): IO[Unit] =
     IO(f.onSuccess { case r => a(r).unsafePerformIO })
 
-  def isComplete:IO[Boolean] = 
+  def isComplete: IO[Boolean] =
     IO(f.isCompleted)
-  
+
 }
 
 object IOFuture {
-
+  
   def newIOFuture[A](a: IO[A])(implicit ec: ExecutionContext): IO[IOFuture[A]] =
     IO(new IOFuture(Future(a.unsafePerformIO)))
 
   implicit class pimpIO[A](a: IO[A])(implicit ec: ExecutionContext) {
     def future: IO[IOFuture[A]] = newIOFuture(a)
   }
-  
+
   implicit object BindIOFuture extends Bind[IOFuture] {
     def bind[A, B](fa: IOFuture[A])(f: A => IOFuture[B]): IOFuture[B] = fa flatMap f
     def map[A, B](fa: IOFuture[A])(f: A => B): IOFuture[B] = fa map f
@@ -45,6 +45,7 @@ object IOFuture {
 
 }
 
+// An example
 object IOFutureTest extends App {
 
   import IOFuture._
@@ -57,10 +58,11 @@ object IOFutureTest extends App {
     c2 <- getChar.future
 
     // All of the onComplete calls can run in any order
-    _ <- c1.onComplete(c => putStrLn("(1) c1 got " + c))
-    _ <- c1.onComplete(c => putStrLn("(2) c1 got " + c))
-    _ <- c2.onComplete(c => putStrLn("c2 got " + c))
+    _ <- c1.onSuccess(c => putStrLn("(1) c1 got " + c))
+    _ <- c1.onSuccess(c => putStrLn("(2) c1 got " + c))
+    _ <- c2.onSuccess(c => putStrLn("c2 got " + c))
 
+    // IOFuture composes
     pair = for {
       a <- c1
       b <- c2
